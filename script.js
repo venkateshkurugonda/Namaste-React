f63404f6fb670379141d9c1596c5079382570221
venkatesh kurugonda <venkateshkurugonda5@mail.com>
2024-08-02 14:28:07 +0530

    episode -1

---
+++ script.js
@@ -0,0 +1,4 @@
+const parent = React.createElement("div", { id: "parent" }, [React.createElement("div", { id: "child1" }, [React.createElement("h1", {}, "I'm a h1 tag"), React.createElement("h2", {}, "I'm a h2 tag")]), React.createElement("div", { id: "child2" }, [React.createElement("h1", {}, "I'm a h1 tag"), React.createElement("h2", {}, "I'm a h2 tag")])])
+const root = ReactDOM.createRoot(document.querySelector("#root"));
+
+root.render(parent);
